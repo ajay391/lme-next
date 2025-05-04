f22ba9264faa6c3f348abed095b49ff9d1f5ae13
ajay391 <prajay391@gmail.com>
2025-05-04 20:28:31 +0530

    changed next config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,10 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//    eslint: {
-//     ignoreDuringBuilds: true,
-//    },
-// };
-
-// export default nextConfig;
-
-const nextConfig = {
-  output: 'export',
+const nextConfig: NextConfig = {
+  /* config options here */
+   eslint: {
+    ignoreDuringBuilds: true,
+   },
 };
 
-module.exports = nextConfig;
-
+export default nextConfig;
